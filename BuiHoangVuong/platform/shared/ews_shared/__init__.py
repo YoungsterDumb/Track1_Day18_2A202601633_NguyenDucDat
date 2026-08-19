c96e6ea9c5587c71ee25e99ab65c7f5b4f6758cd
David Nguyen <108014847+YoungsterDumb@users.noreply.github.com>
2026-08-19 21:51:18 +0700

    Delete BuiHoangVuong directory

--- BuiHoangVuong/platform/shared/ews_shared/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Scoring rules shared by the API and the Celery worker."""
-
-from .explain import explain
-from .features import build_features, trend_of, failed_of
-from .rules import calculate_risk_score, categorize
-
-__all__ = ["explain", "build_features", "trend_of", "failed_of", "calculate_risk_score", "categorize"]
